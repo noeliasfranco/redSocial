72183ec6a7d09a9de12744be7de79b894f330d4c
sofia <pacifico.sofi@gmail.com>
2013-11-20 01:14:16 -0300

    Album.view done

--- public/js/views/lofAlbumView.js
+++ public/js/views/lofAlbumView.js
@@ -11,30 +11,49 @@
 		initialize: function(){
 		this.jQel = $(this.el);
       	_.bindAll(this, 'render', 'album', 'nextPage', 'prevPage'); // fixes loss of context for 'this' within methods
+    	
+    	/*render: function(){
+    		this.jQel.empty();
+			
+			var instance = this;
+			console.log(instance.model.data.length);
+			
+    		var ulist = document.createElement('ul');
+    		*/
+		 this.render(); // not all views are self-rendering. This one is.
     	},
     	
     	render: function(){
     		this.jQel.empty();
-			
 			var instance = this;
-            console.log(instance.model.data.length);
-			
-    		var ulist = document.createElement('ul');
-    		
+            
+            if(instance.model.data.length<=5){
+			     var ancho = 220*instance.model.data.length;
+                $('#lofBody').width(ancho);
+              
+			}else {
+			     var splitFriends = parseInt(instance.model.data.length/2);
+                 $('#lofBody').width(300*splitFriends);
+                 
+			}
+            
+            var albums =  document.createElement('div');
+            albums.className += "container-album";
+            var albumUl = document.createElement('ul');
+            
     		for (a in this.model.data)
     		{
+                
     			if (this.model.data[a].count)
     			{
-    				var albumLi = document.createElement('li');
-    				albumLi.setAttribute('data-album-id', this.model.data[a].id);
-    				albumLi.innerHTML = this.model.data[a].name.toUpperCase();
-	    			//var albumLi = "<li data-album-id=" + this.model.data[a].id + ">";
-	    			//albumLi += this.model.data[a].name.toUpperCase();
-	    			//albumLi += "</li>";
-	    			ulist.appendChild(albumLi);
-    			}
+                    var albumLi = document.createElement('li');
+                    albumLi.setAttribute('data-album-id', this.model.data[a].id);
+                    albumLi.innerHTML = this.model.data[a].name.toUpperCase();
+                    albumUl.appendChild(albumLi);
+                    albums.appendChild(albumUl);    			
+                }
     		}
-    		this.jQel.append(ulist);
+    		this.jQel.append(albums);
     		
     		if(this.model.paging.previous)
     		{
